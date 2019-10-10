ceab9e5e369680cdfe3eeded42ece77dc696308b
Edixon Alberto <edixonalbertto@gmail.com>
2019-10-10 02:13:11 -0400

    mejorando el servidor
    - se implementan middlewares
    - se mejora la estructura del codigo
    - se instalan librerias para el desarrollo y para la conexion
      con la base de datos
    - se instalan types para interpretar dependencias

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,44 @@
-import express from 'express';
-import path from 'path';
+import express, { Application } from 'express';
+import morgan from 'morgan';
+import { resolve as pathResolve } from 'path';
 
-import apiRoutes from './routes';
+/* ROUTES */
+import conceptsRoutes from './routes/concepts.routes';
 
 class Server {
-    private app: express.Application;
-    private static PORT: number;
+	private app: Application;
+	private static PORT: number;
+	private static NODE_ENV: string | undefined;
+	private static PATH_STATIC: string;
 
-    public constructor() {
-        this.app = express();
-        this.settings();
-        this.middleware();
-        this.routes();
-    }
+	public constructor() {
+		this.app = express();
+		this.settings();
+		this.middlewares();
+		this.routes();
+	}
 
-    private settings() {
-        Server.PORT = Number(process.env.PORT) || 4000;
-        this.app.set('PUBLICT', path.join(__dirname, 'public'));
-    }
+	private settings(): void {
+		Server.PORT = Number(process.env.PORT) || 3000;
+		Server.NODE_ENV = process.env.NODE_ENV;
+		Server.PATH_STATIC = pathResolve('public');
+	}
 
-    private middleware() {
-        this.app.use(express.static(this.app.get('PUBLICT')));
-    }
+	private middlewares(): void {
+		this.app.use(express.json());
+		this.app.use(express.urlencoded({ extended: false }));
+		Server.NODE_ENV === 'development' ? this.app.use(morgan('dev')) : null;
+		this.app.use(express.static(Server.PATH_STATIC));
+	}
 
-    private routes() {
-        this.app.use('/api', apiRoutes);
-    }
+	private routes(): void {
+		this.app.use('/api', conceptsRoutes);
+	}
 
-    public async start(port = Server.PORT): Promise<void> {
-        await this.app.listen(port);
-        console.log(`Server on port ${port}`);
-    }
+	public async start(port: number = Server.PORT): Promise<void> {
+		await this.app.listen(port);
+		console.log(`>> Server OK - listening at port: ${port}`);
+	}
 }
 
 export default new Server;
